936af560b0eab1609a38c82adc6eebe500882d62
Gena Makhomed <makhomed@gmail.com>
2016-07-02 18:10:48 +0300

    ...

--- autosnap.go
+++ autosnap.go
@@ -5,21 +5,25 @@ import (
 	"os"
 	"config"
 	"snapman"
+	"flag"
 )
 
-const Config = "/opt/autosnap/conf/autosnap.conf"
+//const Config = "/opt/autosnap/conf/autosnap.conf"
+
+var configName = flag.String("c", "/opt/autosnap/conf/autosnap.conf", "config")
 
 func main() {
-	conf, err := config.New(Config)
+	flag.Parse()
+	conf, err := config.New(*configName)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error parsing config '%s' : %v\n", Config, err)
+		fmt.Fprintf(os.Stderr, "error parsing config '%s' : %v\n", *configName, err)
 		os.Exit(2)
 	}
-	if len(os.Args) == 1 || len(os.Args) == 2 && os.Args[1] == "-h" || len(os.Args) > 2 {
-		fmt.Fprintf(os.Stderr, "usage: %s command|clean\n", os.Args[0])
+	if len(flag.Args()) == 0 || len(flag.Args()) > 1 {
+		fmt.Fprintf(os.Stderr, "usage: %s [-c path/to/config] <command>|clean\n", os.Args[0])
 		os.Exit(2)
 	}
-	command := os.Args[1]
+	command := flag.Arg(0)
 	if _, ok := conf.Interval[command]; !ok && command != "clean" {
 		fmt.Fprintf(os.Stderr, "unknown command '%s', interval not defined\n", command)
 		os.Exit(2)
